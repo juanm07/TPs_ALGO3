74d2ca9850917f898bcd6d72c6cf2173b51b837b
viaguirre <93285792+viaguirre@users.noreply.github.com>
2023-08-31 23:12:59 -0300

    subo versión del ej2 que pasa el juez

--- TP1/ej2.cpp
+++ TP1/ej2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <tuple>
 using namespace std;
 
 
@@ -16,37 +15,41 @@ long long int descontentoTotal(vector<int> &M, vector<int> &C, vector<int> &orde
 }
 
 long long int correccionesPendientes(vector<int> &M, vector<int> &C){
-    vector<pair<double, int> > t(C.size());
+    vector<pair<double,int>> t(C.size());
     for(int i=0; i<C.size();i++){
-        double descontento_prom = C[i]/M[i];
-        t[i] = make_pair(descontento_prom,i);
+        double descontento_prom = static_cast<double>(C[i]) / M[i]; //calculo el coef de descontento en relación al tiempo
+        t[i] = make_pair(descontento_prom,i); //me guardo el resultado junto al número de examen en una tupla
     }
-    sort(t.begin(), t.end(), greater<pair<int, int> >());
+    sort(t.begin(), t.end(), greater<pair<double,int>>()); //ordeno de manera decreciente según el primer valor de la tupla
 
     vector<int> orden(C.size());
     for(int i=0; i< C.size();i++){
-        orden[i] = t[i].second;
+        orden[i] = t[i].second; //orden en el cual se deben corregir los exámenes
     }
     return descontentoTotal(M, C, orden)%1000000007;
 }
 
 int main() {
     int tests;
-    std::cin >> tests;
+    cin >> tests;
 
     while(tests>0){
         int N;
         cin >> N; // cantidad de parciales para corregir
 
         vector<int> M(N);
-        vector<int> C(N); 
-        
-        for (int i = 0; i < N; ++i) {
-            cin >> M[i]; // minutos que tarda en corregir cada parcial
+        vector<int> C(N);
+
+        int min;
+        for (int i = 0; i < N; i++) {
+            cin >> min; //minutos que tarda en corregir cada parcial
+            M[i] = min;
         }
 
-        for (int i = 0; i < N; ++i) {
-            cin >> C[i]; // coeficientes de descontento
+        int coef;
+        for (int i = 0; i < N; i++) {
+            cin >> coef; //coeficientes de descontento
+            C[i] = coef;
         }
 
         cout << correccionesPendientes(M, C) << endl;
