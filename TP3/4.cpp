8ac9fc8489e3ba703e473b9234b36f3b839485a2
viaguirre <93285792+viaguirre@users.noreply.github.com>
2023-11-04 16:01:54 -0300

    Update 4.cpp

--- TP3/4.cpp
+++ TP3/4.cpp
@@ -1,33 +1,32 @@
-
 #include <iostream>
 #include <vector>
 #include <queue>
 using namespace std;
 
-int INF = 1e7;
+long long INF = 1e17;
 
 int n;
 int cant_personas;
 
-vector<vector<int>> capacity;
-vector<vector<int>> capacidades_original;
+vector<vector<long long>> capacity;
+vector<vector<long long>> capacidades_original;
 vector<vector<int>> adj;
 
-int bfs(int s, int t, vector<int>& parent) {
+long long bfs(int s, int t, vector<int>& parent) {
     fill(parent.begin(), parent.end(), -1);
     parent[s] = -2;
-    queue<pair<int, int>> q;
+    queue<pair<int, long long>> q;
     q.push({s, INF});
 
     while (!q.empty()) {
         int cur = q.front().first;
-        int flow = q.front().second;
+        long long flow = q.front().second;
         q.pop();
 
         for (int next : adj[cur]) {
             if (parent[next] == -1 && capacity[cur][next]) {
                 parent[next] = cur;
-                int new_flow = min(flow, capacity[cur][next]);
+                long long new_flow = min(flow, capacity[cur][next]);
                 if (next == t)
                     return new_flow;
                 q.push({next, new_flow});
@@ -38,10 +37,10 @@ int bfs(int s, int t, vector<int>& parent) {
     return 0;
 }
 
-int maxflow(int s, int t) {
-    int flow = 0;
+long long maxflow(int s, int t) { // E-K de la catedra
+    long long flow = 0;
     vector<int> parent(n);
-    int new_flow;
+    long long new_flow;
 
     while ((new_flow = bfs(s, t, parent))){
         flow += new_flow;
@@ -56,23 +55,24 @@ int maxflow(int s, int t) {
     return flow;
 }
 
-int envios(int desde, int hasta){
-    int res = 0;
+long long envios(long long desde, long long hasta){ // hago una busqueda binaria entre la cantidad de herramientas que puede llevar cada persona
+    long long res = 0;
 
-    while(hasta - desde > 1){
-        int mitad = (hasta+desde)/2;
+    while(hasta >= desde){
+        long long mitad = (hasta+desde)/2;
         for(int i=0;i<n; i++){
             for(int j=0;j<n;j++){
-                capacity[i][j] = capacidades_original[i][j]/mitad;
+                capacity[i][j] = capacidades_original[i][j]/mitad; //divido las aristas por la cant de herramientas
             }
         }
-        int max_actual = maxflow(0, n-1);
+        long long max_actual = maxflow(0, n-1); // el flujo máximo seria la cant de personas que pueden llevar X (mitad) herramientas
+        long long cant_herramientas_actual = cant_personas*mitad; //cant total de herramientas que se pueden transportar
 
-        if(max_actual >= cant_personas && max_actual*mitad > res){
-            res = max_actual*mitad;
-            desde  = mitad;
+        if(max_actual >= cant_personas && cant_herramientas_actual > res){
+            res = cant_herramientas_actual;
+            desde  = mitad + 1;
         }else{
-            hasta = mitad;
+            hasta = mitad - 1;
         }
     }
 
@@ -99,9 +99,9 @@ int main(){
         n = N;
 
         vector<vector<int>> grafo(N);
-        vector<vector<int>> capacidades(N, vector<int>(N, 0));
+        vector<vector<long long>> capacidades(N, vector<long long>(N, 0));
 
-        int max_capacidad = 0;
+        long long max_capacidad = 0;
 
         for (int i = 0; i < M; i++) {
             int v,w,c;
@@ -110,15 +110,15 @@ int main(){
             grafo[v - 1].push_back(w-1);
             capacidades[v-1][w-1] = c;
 
-            max_capacidad = max(max_capacidad, c);
+            if(c>max_capacidad) max_capacidad = c; // max_capacidad va a ser mi cota superior para la busqueda binaria
         }
 
         adj = grafo;
         capacidades_original = capacidades;
 
-        capacity.assign(N, vector<int>(N,0));
+        capacity.assign(N, vector<long long>(N,0));
 
-        int res = envios(1, max_capacidad);
+        long long res = envios(1, max_capacidad);
 
         cout << res << endl;
         tests--;
